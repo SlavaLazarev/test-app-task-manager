ab38465c87f51850b7be8967c8275c25657f6a94
SlavaLazarev <creatorishere@icloud.com>
2021-10-12 00:28:51 +0700

    test commit1

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 app.use(express.static('./dist/test-app-task-manager'));
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/test-app-task-manager/'});
+  res.sendFile('index.html', {root: './dist/test-app-task-manager/'});
 });
 app.listen(process.env.PORT || 8080);
